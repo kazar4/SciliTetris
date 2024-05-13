8d5a2b21ec211c949b93814c7547d4cede046ab8
Kazen Gallman <kazar444@icloud.com>
2024-05-13 16:39:55 -0400

    back to sending json

--- commands.py
+++ commands.py
@@ -154,7 +154,8 @@ class Commands:
 
             # I think for now I actually want this to send to client
             # self.sendServerGracefully(server, client, json.dumps({"pong": client["id"], "timeDif": int(timeDif * 1000)}))
-            self.sendServerGracefully(server, client, "Mili Delay:" + str(timeDif * 1000))
+            # self.sendServerGracefully(server, client, "Mili:" + str(timeDif * 1000))
+            self.sendServerGracefully(server, client, json.dumps({"time": int((time.time() * 1000000)/1000), "timeDif": int((timeDif * 1000000)/1000)}))
         else: # TODO
             # server.send_message(client, json.dumps({"ERROR": f"{str(client['id'])} did not send a ping"}))
             self.sendServerGracefully(server, client, json.dumps({"ERROR": f"{str(client['id'])} did not send a ping"}))
