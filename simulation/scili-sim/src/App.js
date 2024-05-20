acf1ef365e7f83a532b60160b1832af662721937
Kazen Gallman <kazar444@icloud.com>
2024-05-20 07:01:08 -0400

    testing other server

--- simulation/scili-sim/src/App.js
+++ simulation/scili-sim/src/App.js
@@ -57,8 +57,8 @@ function App() {
 
 
   useEffect(() => {
-    // const websocket = new WebSocket('wss://kazar4.com:9001');
-    const websocket = new WebSocket('ws://localhost:9001')
+     const websocket = new WebSocket('ws://brown.proteinarium.edu:9001');
+    //const websocket = new WebSocket('ws://localhost:9001')
 
     websocket.onopen = () => {
       console.log('WebSocket connection established.');
