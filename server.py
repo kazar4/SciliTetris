f0ae271b91dda822cc6fd9252f709240352ed10d
Kazen Gallman <kazar444@icloud.com>
2024-05-20 07:02:40 -0400

    server changedd ssl

--- server.py
+++ server.py
@@ -142,7 +142,8 @@ def message_received(client, server, message):
 
 PORT=9001
 # server = WebsocketServer(host='0.0.0.0', port=PORT, key="/ssl/server.key", cert="/ssl/server.crt")
-server = WebsocketServer(host='localhost', port=PORT)
+server = WebsocketServer(host='0.0.0.0', port=PORT)
+# server = WebsocketServer(host='localhost', port=PORT)
 
 commands.start_ping_thread(server)
 
